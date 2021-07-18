ccf5e7163351995179a077283e342aa211944d29
Petunia Ann <46258371+p2llc@users.noreply.github.com>
2021-07-17 15:36:07 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const car = {
+  make: "Chevrolet",
+  model: "Aveo",
+  year: "2006",
+  color: "blue",
+  transmission: "manual",
+  drivetrain: "FWD",
+  doors: 5,
+  };
+  
+  console.log("This is a", car.color);
+  console.log(car.year);
+  console.log(car.make);
+  console.log(car.model);
